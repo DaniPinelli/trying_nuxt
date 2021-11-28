1a0918df64b331de07836034ab475d37c7c6e873
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-11-28 11:27:57 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="html">
+    <h1>Hello World</h1>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default ({
+    name: 'Index'
+})
+</script>
+
+<style scoped>
+
+</style>
