ab94427ad7816b61d824cee30a911344066f5924
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2021-11-28 16:22:19 -0300

    Using nuxt-link

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -4,6 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _23acf222 = () => interopDefault(import('..\\pages\\albums\\_id.vue' /* webpackChunkName: "pages/albums/_id" */))
 const _3bc65caa = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
@@ -18,6 +19,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/albums/:id?",
+    component: _23acf222,
+    name: "albums-id"
+  }, {
     path: "/",
     component: _3bc65caa,
     name: "index"
